7c9e3197b9481ae4676db3f0a278469725bcbeea
Archontis Kostis <arxontisk02@gmail.com>
2023-05-10 17:57:01 +0300

    Update User.java

--- src/main/java/uom/backend/physioassistant/models/users/User.java
+++ src/main/java/uom/backend/physioassistant/models/users/User.java
@@ -1,5 +1,7 @@
 package uom.backend.physioassistant.models.users;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,8 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter
+@MappedSuperclass
 public abstract class User {
+    @Column(nullable = false)
     private String password;
 }
