6bc4c5f8128203cb488e9f7f553a5ddc3eeb6631
Archontis Kostis <arxontisk02@gmail.com>
2023-05-10 15:12:28 +0300

    spring intilizr

---
+++ src/main/java/uom/backend/physioassistant/PhysioAssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package uom.backend.physioassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysioAssistantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysioAssistantBackendApplication.class, args);
+	}
+
+}
